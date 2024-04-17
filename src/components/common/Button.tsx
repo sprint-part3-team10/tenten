8e844e82cfc0618ff22ac62ce4de9827e362547c
Hooni07 <gkxogns21@gmail.com>
2024-04-17 18:04:38 +0900

    Feat: Button 컴포넌트 구현

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,3 +1,16 @@
-const button = () => <div>button</div>;
+import styles from './Button.module.scss';
 
-export default button;
+type ButtonProps = {
+  buttonType: 'button' | 'submit' | 'reset' | undefined;
+  text: string;
+  size: 'L' | 'M' | 'S';
+};
+
+export default function ({ buttonType, text, size }: ButtonProps) {
+  const styleSize = `button${size}`;
+  return (
+    <button className={styles[styleSize]} type={buttonType}>
+      {text}
+    </button>
+  );
+}
