2ee782f33a27b75d4fac2b24043d19fc2150c184
Hooni07 <gkxogns21@gmail.com>
2024-04-17 19:14:32 +0900

    Design: 반응형 동작 추가, white 배경 case 추가, disable case 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,13 +3,31 @@ import styles from './Button.module.scss';
 type ButtonProps = {
   buttonType: 'button' | 'submit' | 'reset' | undefined;
   text: string;
-  size: 'L' | 'M' | 'S';
+  size?: 'L' | 'M' | 'S' | '';
+  isWhite?: boolean;
+  isDisable?: boolean;
 };
 
-export default function ({ buttonType, text, size }: ButtonProps) {
-  const styleSize = `button${size}`;
+export default function ({
+  buttonType,
+  text,
+  size = '',
+  isWhite = false,
+  isDisable = false,
+}: ButtonProps) {
+  let styleSize: string;
+  if (isWhite) {
+    styleSize = size ? `button${size}White` : 'commonButtonWhite';
+  } else {
+    styleSize = size ? `button${size}` : 'commonButton';
+  }
+
   return (
-    <button className={styles[styleSize]} type={buttonType}>
+    <button
+      className={styles[styleSize]}
+      type={buttonType}
+      disabled={isDisable}
+    >
       {text}
     </button>
   );
