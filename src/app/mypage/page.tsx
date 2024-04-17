c0c0df227fc75b2bc523fbf6df3ae44f735b9609
eugene9851 <eugenelee980713@gmail.com>
2024-04-17 15:00:44 +0900

    Design: MyProfile 컴포넌트(버튼 제외)

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,12 +1,20 @@
-import ApplyTable from '@/components/applyList/ApplyTable';
-import NoApply from '@/components/applyList/NoApply';
+import ApplyTable from '@/src/components/applyList/ApplyTable';
+import NoList from '@/src/components/applyList/NoList';
+import MyProfile from '@/src/components/myProfile/MyProfile';
+import styles from './page.module.scss';
 
 function mypage() {
-  const applyNum = 1;
-  return applyNum ? (
-    <ApplyTable />
-  ) : (
-    <NoApply description={'아직 신청 내역이 없어요.'} />
+  return (
+    <>
+      <div className={styles.section}>
+        <h1 className={styles.title}>내 프로필</h1>
+        <MyProfile />
+      </div>
+      <NoList
+        description={'아직 신청 내역이 없어요.'}
+        buttonAction={'공고 보러가기'}
+      />
+    </>
   );
 }
 
---
+++ src/components/myProfile/MyProfile.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import styles from './MyProfile.module.scss';
+import phone from '@/public/icons/phone.svg';
+import location from '@/public/icons/location.svg';
+
+const mockData = {
+  name: '김승우',
+  phone: '010-1234-5678',
+  address: '서울시 도봉구',
+  bio: '열심히 일 하겠습니다',
+};
+
+export default function MyProfile() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.profileInfo}>
+        <h1 className={styles.name}>이름</h1>
+        <h2 className={styles.nameText}>{mockData.name}</h2>
+        <div>
+          <Image
+            width={20}
+            height={20}
+            src={phone}
+            alt='phone'
+            className={styles.icon}
+          />
+          <span className={styles.phone}>{mockData.phone}</span>
+        </div>
+        <div>
+          <Image
+            width={20}
+            height={20}
+            src={location}
+            alt='area'
+            className={styles.icon}
+          />
+          <span className={styles.address}>선호 지역: {mockData.address}</span>
+        </div>
+        <p className={styles.bio}>{mockData.bio}</p>
+      </div>
+      {/* 버튼 컴포넌트 */}
+      <button>편집하기</button>
+    </div>
+  );
+}
