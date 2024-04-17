fa617ebec9658c82975bb495b29ed2816ad87b0b
hanbin <rj2303@naver.com>
2024-04-17 19:41:22 +0900

    Feat: 더미 객체 데이터 참조하도록 컴포넌트 수정

--- src/components/store/JobDescription.tsx
+++ src/components/store/JobDescription.tsx
@@ -1,14 +1,15 @@
 import styles from './JobDescription.module.scss';
 
-function JobDescription() {
+interface Props {
+  description: string;
+}
+
+function JobDescription({ description }: Props) {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>공고 설명</h2>
       <br />
-      <p>
-        기존 알바 친구가 그만둬서 새로운 친구를 구했는데, 그 사이에 하루가
-        비네요. 급해서 시급도 높였고 그렇게 바쁜 날이 아니라서 괜찮을거예요.
-      </p>
+      <p>{description}</p>
     </div>
   );
 }
--- src/components/store/ShopNoticeInfoBox.tsx
+++ src/components/store/ShopNoticeInfoBox.tsx
@@ -1,18 +1,54 @@
 import Image from 'next/image';
 import styles from './ShopNoticeInfoBox.module.scss';
 import WageComparisonBadge from '../common/WageComparisonBadge';
+import formatWage from '@/src/lib/formatWage';
+
+interface ShopData {
+  item: {
+    id: string;
+    name: string;
+    category: string;
+    address1: string;
+    address2: string;
+    description: string;
+    imageUrl: string;
+    originalHourlyPay: number;
+  };
+}
+
+// interface User {
+//   item: {
+//     id: string;
+//     email: string;
+//     type: string;
+//   };
+//   href: string;
+// }
 
 interface Props {
   term: boolean;
+  noticeData: {
+    id: string;
+    hourlyPay: number;
+    startsAt: string;
+    workhour: number;
+    description: string;
+    closed: boolean;
+    shop: ShopData & {
+      href: string;
+    };
+    currentUserApplication: null;
+  };
+  // shopData?: (ShopData & User) | null;
 }
 
-function ShopNoticeInfoBox(term: Props) {
+function ShopNoticeInfoBox({ term, noticeData }: Props) {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
         <Image
           fill
-          src='/examplePlaceImage.png'
+          src={noticeData.shop.item.imageUrl}
           alt='식당 이미지'
           className={styles.image}
         />
@@ -21,17 +57,15 @@ function ShopNoticeInfoBox(term: Props) {
         <div className={styles.textInfo}>
           <p className={styles.category}>시급</p>
           <div className={styles.title}>
-            <span>15,000원</span>
+            <span>{formatWage(noticeData.hourlyPay)}</span>
             <WageComparisonBadge />
           </div>
           {term && (
             <div className={styles.term}>2023-01-02 15:00~18:00 (3시간)</div>
           )}
-          <div className={styles.address}>서울시 송파구</div>
+          <div className={styles.address}>{noticeData.shop.item.address1}</div>
           <div className={styles.description}>
-            알바하기 편한 너구리네 라면집!
-            <br />
-            라면 올려두고 끓이기만 하면 되어서 쉬운 편에 속하는 가게입니다.
+            {noticeData.shop.item.description}
           </div>
         </div>
         <button className={styles.button} type='button'>
