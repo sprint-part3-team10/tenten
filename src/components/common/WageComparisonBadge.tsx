724c7ac041430fcfbcbae5ff639c254a6601dbfb
hanbin <rj2303@naver.com>
2024-04-18 14:46:04 +0900

    Style: 이미지 import 방식 수정

--- src/components/common/WageComparisonBadge.tsx
+++ src/components/common/WageComparisonBadge.tsx
@@ -1,16 +1,12 @@
 import Image from 'next/image';
 import styles from './WageComparisonBadge.module.scss';
+import arrow from '@/public/icons/wageArrow.svg';
 
 function WageComparisonBadge() {
   return (
     <div className={styles.badge}>
       기존 시급보다 50%
-      <Image
-        width={20}
-        height={20}
-        src='/icons/wageArrow.svg'
-        alt='시급 비교 화살표'
-      />
+      <Image width={20} height={20} src={arrow} alt='시급 비교 화살표' />
     </div>
   );
 }
