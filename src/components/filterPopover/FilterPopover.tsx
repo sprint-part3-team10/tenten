0073cfa69255222b8bdf34c83633792d145b8463
ggjiny <jnss599@gmail.com>
2024-04-19 01:49:26 +0900

    Design: FilterPopover 초기 디자인

--- src/components/filterPopover/FilterPopover.tsx
+++ src/components/filterPopover/FilterPopover.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './Filter.module.scss';
 
 function FilterPopover() {
-  return <div>FilterPopOver</div>;
+  return (
+    <div className={styles.popover}>
+      <h1>상세 필터</h1>
+    </div>
+  );
 }
 
 export default FilterPopover;
