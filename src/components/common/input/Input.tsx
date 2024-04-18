036b2819dd51c63c68a8d8ce5da66041cfe6550e
Hooni07 <gkxogns21@gmail.com>
2024-04-18 23:31:25 +0900

    Feat: eye 버튼 클릭 시, 비밀번호가 보이게 하는 기능 추가

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,10 +1,14 @@
+'use client';
+
 import classNames from 'classnames';
 import { FieldError, UseFormRegisterReturn } from 'react-hook-form';
 import styles from './Input.module.scss';
+import Image from 'next/image';
+import { useState } from 'react';
 
 interface InputProps {
   label: string;
-  inputType?: 'text' | 'email' | 'password';
+  inputType?: 'email' | 'text' | 'password';
   value?: string | number;
   widthValue?: string;
   error?: FieldError;
@@ -21,7 +25,19 @@ export default function Input({
   register,
   isStatic = true,
 }: InputProps) {
+  const [isVisible, setIsVisible] = useState(false);
   const isError: boolean = !!error;
+
+  if (inputType === 'password') {
+    if (isVisible) {
+      inputType = 'text';
+    }
+  }
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <div
       className={styles.inputContainer}
@@ -45,6 +61,25 @@ export default function Input({
           placeholder='입력'
           {...register}
         />
+        {(label === '비밀번호' || label === '비밀번호 확인') && (
+          <button onClick={toggleVisibility} className={styles.eye}>
+            {isVisible ? (
+              <Image
+                src='icons/eyeon.svg'
+                width={16}
+                height={16}
+                alt='비밀번호 보기'
+              />
+            ) : (
+              <Image
+                src='icons/eyeoff.svg'
+                width={16}
+                height={16}
+                alt='비밀번호 숨기기'
+              />
+            )}
+          </button>
+        )}
         {label === '시급*' && <span className={styles.won}>원</span>}
       </div>
 
