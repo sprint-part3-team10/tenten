ee1bcf04340eac9d0bd51017055a2af45b9f05ca
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-04-18 16:01:29 +0900

    Style: widthValue prop 추가(width 설정 가능) 및 classNames 모듈 적용

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,32 +1,33 @@
+import classNames from 'classnames';
 import styles from './Button.module.scss';
 
 interface ButtonProps {
   buttonType: 'button' | 'submit' | 'reset' | undefined;
   text: string;
   size?: 'L' | 'M' | 'S' | '';
+  widthValue?: string;
   isWhite?: boolean;
   isDisable?: boolean;
 }
 
-export default function ({
+export default function Button({
   buttonType,
   text,
   size = '',
+  widthValue = '100%',
   isWhite = false,
   isDisable = false,
 }: ButtonProps) {
-  let styleSize: string;
-  if (isWhite) {
-    styleSize = size ? `button${size}White` : 'commonButtonWhite';
-  } else {
-    styleSize = size ? `button${size}` : 'commonButton';
-  }
-
   return (
     <button
-      className={styles[styleSize]}
+      className={classNames(
+        styles.commonButton,
+        { [styles.white]: isWhite },
+        { [styles[size]]: size },
+      )}
       type={buttonType}
       disabled={isDisable}
+      style={{ width: widthValue }}
     >
       {text}
     </button>
