32198a664e219f0937bc7e30e730c7a2a4aeff57
hanbin <rj2303@naver.com>
2024-04-19 23:29:00 +0900

    Design: 버튼, 모달 사이즈 반응형 구현

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -7,35 +7,50 @@ import warningIcon from '@/public/icons/modalWarning.svg';
 import checkIcon from '@/public/icons/modalCheck.svg';
 import useOutsideClick from '@/src/hooks/useOutsideClick';
 import Button from '../Button';
+import { min } from 'date-fns';
 
 interface ModalProps {
   icon?: 'warning' | 'check';
   message: string;
+  minWidth: string;
+  maxWidth: string;
   buttonText: [string] | [string, string];
+  buttonWidthPercent?: string;
   handleModal: (value: boolean) => void;
-  buttonWidth: string;
 }
 
 /**
  * 모달 컴포넌트
- * @param icon 모달에 체크나 느낌표 아이콘을 띄울지 여부
- * @param {string} message 모달 메세지
- * @param {array} buttonText 버튼에 쓸 글자를 배열로 전달. 배열 길이에 따라 버튼 갯수가 정해집니다 (두 개까지)
- * @param handleModal 클릭 시 닫히도록 할 setState 함수
+ * @param props
+ * @param props.icon 모달에 체크나 느낌표 아이콘을 띄울지 여부
+ * @param {string} props.message 모달 메세지
+ * @param {array} props.buttonText 버튼에 쓸 글자를 배열로 전달. 배열 길이에 따라 버튼 갯수가 정해집니다 (두 개까지)
+ * @param {string} props.buttonWidthPercent 버튼 가로 길이. 패딩을 제외한 모달 길이의 백분율로 정해주세요. ex) 25%
+ * @param props.handleModal 클릭 시 닫히도록 할 setState 함수
  * @returns
  */
 
 function Modal({
   icon,
   message,
+  minWidth,
+  maxWidth,
   buttonText = ['닫기'],
+  buttonWidthPercent = '35%',
   handleModal,
-  buttonWidth,
 }: ModalProps) {
   const ref = useRef<HTMLDivElement>(null);
   const iconSelector = {
     warning: warningIcon,
     check: checkIcon,
+  };
+
+  const modalBoxStyle = {
+    minWidth,
+    maxWidth,
+  };
+  const buttonContainerStyle = {
+    flexBasis: buttonWidthPercent,
   };
 
   const handleClick = () => {
@@ -46,34 +61,33 @@ function Modal({
 
   return (
     <div className={styles.background}>
-      <div ref={ref} className={styles.box}>
+      <div ref={ref} className={styles.box} style={modalBoxStyle}>
         {icon && (
           <Image
+            className={styles.icon}
             width={24}
             height={24}
             src={iconSelector[icon]}
             alt='모달 내부 아이콘'
           />
         )}
-        <div>{message}</div>
+        <div className={styles.message}>{message}</div>
         <div className={styles.buttonContainer}>
           {buttonText.length === 2 && (
-            <div onClick={handleClick}>
+            <div onClick={handleClick} style={buttonContainerStyle}>
               <Button
                 buttonType='button'
                 text={buttonText[0]}
                 size='M'
-                widthValue={buttonWidth}
                 isWhite
               />
             </div>
           )}
-          <div onClick={handleClick}>
+          <div onClick={handleClick} style={buttonContainerStyle}>
             <Button
               buttonType='button'
               text={buttonText[1] || buttonText[0]}
               size='M'
-              widthValue={buttonWidth}
             />
           </div>
         </div>
