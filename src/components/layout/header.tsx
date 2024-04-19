2f27d0591fa142755ca5e32815a4cb4c32f7f21d
JA_X <envijs93@gmail.com>
2024-04-19 21:00:07 +0900

    Feat: Header 컴포넌트 구현 및 적용

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,34 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import SearchBar from '../SearchBar';
+import logo from '@/public/icons/logo.svg';
 import styles from './Header.module.scss';
 
 export default function Header() {
-  return <div>헤더입니당</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerBox}>
+        <Link href='/'>
+          <Image
+            className={styles.logo}
+            src={logo}
+            width={110}
+            height={20}
+            alt='로고'
+          />
+        </Link>
+        <div className={styles.searchBar}>
+          <SearchBar />
+        </div>
+        <div className={styles.activeList}>
+          <Link href='/signin'>
+            <button>로그인</button>
+          </Link>
+          <Link href='/signup'>
+            <button>회원가입</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
