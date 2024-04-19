0f604176cfec9900ec8a2918ff6201593f29262d
ggjiny <jnss599@gmail.com>
2024-04-19 15:54:52 +0900

    Feat: FilterPopover 초기 구현

--- src/components/filterPopover/FilterButton.tsx
+++ src/components/filterPopover/FilterButton.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import styles from './Filter.module.scss';
+import FilterPopover from './FilterPopover';
 
 function FilterButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = (type: string = '') => {
+    if (type === 'filter' && !isOpen) {
+      setIsOpen(true);
+    } else if (type !== 'filter') setIsOpen(false);
+  };
+
   return (
-    <button className={styles.button} type='button'>
-      상세 필터
-    </button>
+    <div className={styles.buttonContainer}>
+      <button
+        className={styles.button}
+        type='button'
+        onClick={() => handleClick('filter')}
+      >
+        상세 필터
+      </button>
+      {isOpen && <FilterPopover onClick={handleClick} />}
+    </div>
   );
 }
 
--- src/components/filterPopover/FilterPopover.tsx
+++ src/components/filterPopover/FilterPopover.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import closeIcon from '@/public/icons/close.svg';
+import Image from 'next/image';
 import styles from './Filter.module.scss';
+import LocationBox from './LocationBox';
 
-function FilterPopover() {
+interface FilterPopoverProps {
+  onClick: () => void;
+}
+
+function FilterPopover({ onClick }: FilterPopoverProps) {
   return (
     <div className={styles.popover}>
-      <h1>상세 필터</h1>
+      <div className={styles.titleContainer}>
+        <h1>상세 필터</h1>
+        <div role='button' className={styles.close} onClick={onClick}>
+          <Image src={closeIcon} width={24} height={24} alt='close popover' />
+        </div>
+      </div>
+      <div className={styles.locationContainer}>
+        <h2>위치</h2>
+        <LocationBox />
+      </div>
     </div>
   );
 }
