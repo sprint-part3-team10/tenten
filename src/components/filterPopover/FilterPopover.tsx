483b8c75c6da6ddd31fc857ccfb020333d7a480f
ggjiny <jnss599@gmail.com>
2024-04-21 01:43:04 +0900

    Feat: DatePicker 사용

--- src/components/filterPopover/FilterPopover.tsx
+++ src/components/filterPopover/FilterPopover.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import closeIcon from '@/public/icons/close.svg';
 import Image from 'next/image';
 import styles from './Filter.module.scss';
 import LocationBox from './LocationBox';
+import DatePicker from '../common/Datepicker';
 
 interface FilterPopoverProps {
   onClick: () => void;
 }
 
 function FilterPopover({ onClick }: FilterPopoverProps) {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const handleChangeDate = (date: Date) => setSelectedDate(date);
+
   return (
     <div className={styles.popover}>
       <div className={styles.titleContainer}>
         <h1>상세 필터</h1>
-        <div role='button' className={styles.close} onClick={onClick}>
+        <div className={styles.close} onClick={onClick}>
           <Image src={closeIcon} width={24} height={24} alt='close popover' />
         </div>
       </div>
       <div className={styles.locationContainer}>
         <h2>위치</h2>
         <LocationBox />
+      </div>
+      <div className={styles.locationContainer}>
+        <h2>시작일</h2>
+        <DatePicker value={selectedDate} onChange={handleChangeDate} />
       </div>
     </div>
   );
