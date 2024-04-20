4f0174f2b0212ec1bc25e107d217c8de5f65a7fc
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-04-20 16:08:29 +0900

    Fixed: Toast 컴포넌트 파일 위치 변경으로 인한 삭제

--- src/components/common/Toast.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Toast.module.scss';
-
-interface ToastProps {
-  message: string;
-}
-
-export default function Toast({ message }: ToastProps) {
-  return <div className={styles.toastContainer}>{message}</div>;
-}
