37e45c1c80303f41754346f270969dde0469d3a1
Hooni07 <gkxogns21@gmail.com>
2024-04-20 11:07:50 +0900

    Feat: Toast 컴포넌트 test 설정 및 useToast custom hook 생성

--- src/components/common/toast/Toast.tsx
+++ src/components/common/toast/Toast.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
 import styles from './Toast.module.scss';
 
 interface ToastProps {
   message: string;
+  duration?: number;
 }
 
-export default function Toast({ message }: ToastProps) {
-  return <div className={styles.toastContainer}>{message}</div>;
+export default function Toast({ message, duration = 3000 }: ToastProps) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, duration);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message, duration]);
+
+  return <div className={styles.toast}>{message}</div>;
 }
