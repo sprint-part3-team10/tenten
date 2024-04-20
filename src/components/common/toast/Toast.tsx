78a624980bdab48fca0a4fd6e14b95b2cf019ec8
Hooni07 <gkxogns21@gmail.com>
2024-04-20 15:57:45 +0900

    Feat: isWarning props 추가, Warning Case 토스트 상황 추가

--- src/components/common/toast/Toast.tsx
+++ src/components/common/toast/Toast.tsx
@@ -1,12 +1,21 @@
 import { useEffect, useState } from 'react';
 import styles from './Toast.module.scss';
+import COMPLETE from '@/public/icons/completed.png';
+import WARNING from '@/public/icons/warning.svg';
+import Image from 'next/image';
+import classNames from 'classnames';
 
 interface ToastProps {
   message: string;
   duration?: number;
+  isWarning?: boolean;
 }
 
-export default function Toast({ message, duration = 3000 }: ToastProps) {
+export default function Toast({
+  message,
+  duration = 3000,
+  isWarning = false,
+}: ToastProps) {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
@@ -18,5 +27,22 @@ export default function Toast({ message, duration = 3000 }: ToastProps) {
     };
   }, [message, duration]);
 
-  return <div className={styles.toast}>{message}</div>;
+  return (
+    <div className={styles.toastContainer}>
+      <div className={styles.toast}>
+        {isWarning ? (
+          <Image src={WARNING} alt='경고' width={26} height={26} />
+        ) : (
+          <Image src={COMPLETE} alt='정상 동작' width={26} height={26} />
+        )}
+        {message}
+      </div>
+      <div
+        className={classNames(styles.progressBar, {
+          [styles.warning]: isWarning,
+        })}
+        style={{ animationDuration: `${duration}ms` }}
+      ></div>
+    </div>
+  );
 }
