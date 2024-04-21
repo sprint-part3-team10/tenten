b1a12a35caff6a4cec49f1d0a12e2268db705368
eugene9851 <eugenelee980713@gmail.com>
2024-04-21 13:54:51 +0900

    Design: layout padding&margin 조정

--- src/app/(main)/users/[userId]/page.tsx
+++ src/app/(main)/users/[userId]/page.tsx
@@ -15,21 +15,37 @@ async function mypage({ params }: MyPageProps) {
 
   const result = await getProfileData(userId);
   const { name, phone, address, bio } = result;
-  const listNum = 0;
+  const listNum = 1;
 
-  return !name ? (
-    <div className={styles.section}>
-      <div className={styles.title}>내 프로필</div>
-      <div className={styles.profileBox}>
-        <MyProfile name={name} phone={phone} address={address} bio={bio} />
-      </div>
-    </div>
-  ) : (
+  return name !== 'string' ? (
     <NoList
       title='내 프로필'
       description='내 프로필을 등록하고 원하는 가게에 지원해 보세요.'
       text='내 프로필 등록하기'
     />
+  ) : (
+    <>
+      <div className={styles.section}>
+        <div className={styles.title}>내 프로필</div>
+        <div className={styles.profileBox}>
+          <MyProfile name={name} phone={phone} address={address} bio={bio} />
+        </div>
+      </div>
+      {listNum ? (
+        <div className={styles.backgroundArea}>
+          <div className={styles.applyArea}>
+            <div className={styles.title}>신청 내역</div>
+            <ApplyTable />
+          </div>
+        </div>
+      ) : (
+        <NoList
+          title='신청 내역'
+          description='아직 신청 내역이 없어요.'
+          text='공고 보러가기'
+        />
+      )}
+    </>
   );
 }
 
