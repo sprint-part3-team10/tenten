3cb85962f7bfdb81d6d2409d291fd96ff71db2c2
eugene9851 <eugenelee980713@gmail.com>
2024-04-23 00:32:05 +0900

    Feat: 내 프로필 상세 페이지 구현

--- src/app/(main)/myprofile/[userId]/page.tsx
+++ src/app/(main)/myprofile/[userId]/page.tsx
@@ -2,7 +2,7 @@ import NoList from '@/src/components/applyList/NoList';
 import MyProfile from '@/src/components/myProfile/MyProfile';
 import ApplyTable from '@/src/components/applyList/ApplyTable';
 import getProfileData from '@/src/api/getProfileData';
-import getUserApplyCount from '@/src/api/getUserApplyCount';
+import getUserApplyCount from '@/src/api/getUserApply';
 import styles from './page.module.scss';
 
 interface MyPageProps {
@@ -10,15 +10,17 @@ interface MyPageProps {
     [param: string]: string;
   };
 }
+// const userType = 'employee' : employee일 때 해당 페이지로 이동
 
 async function mypage({ params }: MyPageProps) {
   const { userId } = params;
 
   const result = await getProfileData(userId);
   const { name, phone, address, bio } = result;
-  const listNum = await getUserApplyCount(userId);
 
-  return name === 'string' ? (
+  const { count, items } = await getUserApplyCount(userId);
+
+  return !name ? (
     <NoList
       title='내 프로필'
       description='내 프로필을 등록하고 원하는 가게에 지원해 보세요.'
@@ -33,10 +35,10 @@ async function mypage({ params }: MyPageProps) {
         </div>
       </div>
       <div className={styles.backgroundArea}>
-        {listNum ? (
+        {count ? (
           <div className={styles.applyArea}>
             <div className={styles.title}>신청 내역</div>
-            <ApplyTable />
+            <ApplyTable totalCount={count} applies={items} />
           </div>
         ) : (
           <NoList
