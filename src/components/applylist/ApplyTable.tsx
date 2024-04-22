28ea618fc689c11ef7b6b9820c3f034c985aa882
eugene9851 <eugenelee980713@gmail.com>
2024-04-23 00:46:18 +0900

    Feat: ApplyTable 컴포넌트 employee, employer일 때 나눠서 로직 구현

--- src/components/applylist/ApplyTable.tsx
+++ src/components/applylist/ApplyTable.tsx
@@ -1,93 +1,124 @@
+/* eslint-disable no-nested-ternary */
+
+'use client';
+
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import classNames from 'classnames';
-import { formatDateAndTime } from '@/src/lib/format';
+import { formatDateAndTime, formatWage } from '@/src/lib/format';
+import { useEffect } from 'react';
+import usePagination from '@/src/hooks/usePagination';
 import Label from './Label';
 import styles from './ApplyTable.module.scss';
+import Pagination from '../pagination/Pagination';
 
-const mockData = {
-  shop: {
+interface ApplyTableProps {
+  totalCount: number;
+  applies: {
     item: {
-      id: '1234',
-      name: '해피 버거',
-      address1: '서울시 은평구',
-    },
-  },
-  notice: {
-    startsAt: '2023-07-07T18:00:00.000Z',
-    workhour: 10,
-    hourlyPay: 30000,
-  },
+      id: string;
+      status: string;
+      user: {
+        item: {
+          name: string;
+          phone: string;
+          address: string;
+          bio: string;
+        };
+      };
+      shop: {
+        item: {
+          name: string;
+        };
+      };
+      notice: {
+        item: {
+          hourlyPay: number;
+          startsAt: string;
+          workhour: number;
+        };
+      };
+    };
+  }[];
+}
+
+const titleCol = {
+  employee: ['가게', '일자', '시급', '상태'],
+  employer: ['신청자', '소개', '전화번호', '상태'],
 };
 
-function ApplyTable() {
+function ApplyTable({ totalCount, applies }: ApplyTableProps) {
+  const LIMIT = 5;
+  const { offset, selectedPage, handlePageChange } = usePagination(LIMIT);
+  useEffect(() => {
+    console.log('offset', offset);
+  }, [offset]);
+  const userType = 'employee'; // 쿠키로 구현 예정
+
   return (
     <div className={styles.tableContainer}>
       <table className={styles.table}>
         <thead>
           <tr className={styles.titleRow}>
-            <th className={classNames(styles.title, styles.nameCol)}>가게</th>
-            <th className={classNames(styles.title, styles.timeCol)}>일자</th>
-            <th className={classNames(styles.title, styles.payCol)}>시급</th>
-            <th className={classNames(styles.title, styles.statusCol)}>상태</th>
+            <th className={classNames(styles.title, styles.nameCol)}>
+              {titleCol[userType][0]}
+            </th>
+            <th className={classNames(styles.title, styles.timeCol)}>
+              {titleCol[userType][1]}
+            </th>
+            <th className={classNames(styles.title, styles.payCol)}>
+              {titleCol[userType][2]}
+            </th>
+            <th className={classNames(styles.title, styles.statusCol)}>
+              {titleCol[userType][3]}
+            </th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className={classNames(styles.listRow, styles.nameCol)}>
-              {mockData.shop.item.name}
-            </td>
-            <td className={classNames(styles.listRow, styles.timeCol)}>
-              {formatDateAndTime(
-                mockData.notice.startsAt,
-                mockData.notice.workhour,
-              )}
-            </td>
-            <td className={classNames(styles.listRow, styles.payCol)}>
-              {mockData.notice.hourlyPay}
-            </td>
-            <td className={classNames(styles.listRow, styles.statusCol)}>
-              <Label labelType='location' content='서울시 도봉구' />
-            </td>
-          </tr>
-          <tr>
-            <td className={classNames(styles.listRow, styles.nameCol)}>
-              {mockData.shop.item.name}
-            </td>
-            <td className={classNames(styles.listRow, styles.timeCol)}>
-              {formatDateAndTime(
-                mockData.notice.startsAt,
-                mockData.notice.workhour,
-              )}
-            </td>
-            <td className={classNames(styles.listRow, styles.payCol)}>
-              {mockData.notice.hourlyPay}
-            </td>
-            <td className={classNames(styles.listRow, styles.statusCol)}>
-              <Label labelType='status' content='accepted' />
-            </td>
-          </tr>
-          <tr>
-            <td className={classNames(styles.listRow, styles.nameCol)}>
-              {mockData.shop.item.name}
-            </td>
-            <td className={classNames(styles.listRow, styles.timeCol)}>
-              {formatDateAndTime(
-                mockData.notice.startsAt,
-                mockData.notice.workhour,
-              )}
-            </td>
-            <td className={classNames(styles.listRow, styles.payCol)}>
-              {mockData.notice.hourlyPay}
-            </td>
-            <td className={classNames(styles.listRow, styles.statusCol)}>
-              <Label labelType='status' content='rejected' />
-            </td>
-          </tr>
+          {applies &&
+            applies.map(apply => (
+              <tr key={apply.item.id}>
+                <td className={classNames(styles.listRow, styles.nameCol)}>
+                  {userType === 'employee'
+                    ? apply.item.shop.item.name
+                    : apply.item.user.item.name}
+                </td>
+                <td className={classNames(styles.listRow, styles.timeCol)}>
+                  {userType === 'employee'
+                    ? formatDateAndTime(
+                        apply.item.notice.item.startsAt,
+                        apply.item.notice.item.workhour,
+                      )
+                    : apply.item.user.item.bio}
+                </td>
+                <td className={classNames(styles.listRow, styles.payCol)}>
+                  {userType === 'employee'
+                    ? formatWage(apply.item.notice.item.hourlyPay)
+                    : apply.item.user.item.phone}
+                </td>
+                <td className={classNames(styles.listRow, styles.statusCol)}>
+                  {userType === 'employee' ? (
+                    <Label labelType='status' content={apply.item.status} />
+                  ) : apply.item.status !== 'pending' ? (
+                    <Label labelType='status' content={apply.item.status} />
+                  ) : (
+                    <>
+                      <button>거절하기</button>
+                      <button>승인하기</button>
+                    </>
+                  )}
+                </td>
+              </tr>
+            ))}
         </tbody>
         <tfoot>
           <tr>
             <td colSpan={4} className={styles.pagination}>
-              1 2 3 4 5
+              <Pagination
+                totalCount={totalCount}
+                limit={LIMIT}
+                selectedPage={selectedPage}
+                handlePageChange={handlePageChange}
+              />
             </td>
           </tr>
         </tfoot>
