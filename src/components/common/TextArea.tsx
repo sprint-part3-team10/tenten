0834fe28a8b51d97eb674626035c4e087f3a975c
JA_X <envijs93@gmail.com>
2024-04-23 05:08:44 +0900

    Feat: TextArea 컴포넌트 textLimit, placeholder prop 추가 및 글자수 제한 로직 구현

--- src/components/common/TextArea.tsx
+++ src/components/common/TextArea.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import { UseFormRegisterReturn, FieldError } from 'react-hook-form';
+import classNames from 'classnames';
 import styles from './TextArea.module.scss';
 
 interface TextAreaProps {
   labelName: string;
+  textLimit: number;
+  placeholder: string;
   value: string;
   onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
   register: UseFormRegisterReturn;
@@ -11,21 +15,44 @@ interface TextAreaProps {
 
 export default function TextArea({
   labelName,
+  textLimit,
+  placeholder,
   register,
   value,
   onChange,
   error,
 }: TextAreaProps) {
+  const [textLength, setTextLength] = useState(0);
+  const handleOnChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (textLength <= textLimit) {
+      onChange(event);
+      const slicedTextLength = event.target.value.slice(0, textLimit).length;
+      setTextLength(slicedTextLength);
+    }
+  };
+  const isTextLengthOver = textLength === textLimit;
   return (
     <div className={styles.textAreaBox}>
       <label htmlFor={labelName}>{labelName}</label>
       <textarea
-        className={styles.textArea}
-        placeholder='자기 소개를 입력해 주세요.'
+        className={classNames(styles.textArea, {
+          [styles.errorBox]: !!error,
+        })}
+        placeholder={placeholder}
+        maxLength={textLimit}
         value={value}
         {...register}
-        onChange={onChange}
+        onChange={handleOnChange}
       />
+      <div className={styles.textLimit}>
+        <p>
+          (
+          <span className={`${isTextLengthOver ? styles.textLengthOver : ''}`}>
+            {` ${textLength} `}
+          </span>
+          / {`${textLimit} `})
+        </p>
+      </div>
       {error && <div className={styles.error}>{error.message}</div>}
     </div>
   );
