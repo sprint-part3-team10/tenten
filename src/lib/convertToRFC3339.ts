7ba5fcda4699af82ee6a5d0bbb6c7db598c2ea36
JA_X <envijs93@gmail.com>
2024-04-23 23:47:43 +0900

    Fix: convertToRFC3339 유틸 함수 수정

--- src/lib/convertToRFC3339.ts
+++ src/lib/convertToRFC3339.ts
@@ -1,17 +1,10 @@
-export default function convertToRFC3339(koreanDateTimeStr: string) {
-  const formattedStr = koreanDateTimeStr.replace(/년|월|일|오전|오후/g, '');
-  const targetDate = new Date(formattedStr);
-
-  if (koreanDateTimeStr.includes('오후')) {
-    targetDate.setHours(targetDate.getHours() + 12);
-  }
-
-  const year = targetDate.getFullYear();
-  const month = String(targetDate.getMonth() + 1).padStart(2, '0');
-  const day = String(targetDate.getDate()).padStart(2, '0');
-  const hours = String(targetDate.getHours()).padStart(2, '0');
-  const minutes = String(targetDate.getMinutes()).padStart(2, '0');
-  const seconds = String(targetDate.getSeconds()).padStart(2, '0');
+export default function convertToRFC3339(dateObject: Date) {
+  const year = dateObject.getUTCFullYear();
+  const month = String(dateObject.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(dateObject.getUTCDate()).padStart(2, '0');
+  const hours = String(dateObject.getUTCHours()).padStart(2, '0');
+  const minutes = String(dateObject.getUTCMinutes()).padStart(2, '0');
+  const seconds = String(dateObject.getUTCSeconds()).padStart(2, '0');
 
   return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
 }
