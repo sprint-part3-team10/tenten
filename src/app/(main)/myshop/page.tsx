719bd30e96c24001a9788fb9b6c21ce4eb543dac
hanbin <rj2303@naver.com>
2024-04-23 17:28:40 +0900

    Feat: 등록된 가게 없을 때 ui 구현

--- src/app/(main)/myshop/page.tsx
+++ src/app/(main)/myshop/page.tsx
@@ -1,23 +1,42 @@
 import getShop from '@/src/api/getShop';
 import ShopNoticeInfoBox from '@/src/components/store/ShopNoticeInfoBox';
 import { cookies } from 'next/headers';
+import Button from '@/src/components/common/Button';
+import styles from './page.module.scss';
 
 export default async function MyShop() {
   const shopId = cookies().get('shopId');
 
-  if (!shopId) return <div />;
+  if (!shopId)
+    return (
+      <section>
+        <h1 className={styles.sectionTitle}>내 가게</h1>
+      </section>
+    );
 
   const { item } = await getShop(shopId.value);
 
   return (
-    <ShopNoticeInfoBox
-      data={{
-        address1: item.address1,
-        description: item.description,
-        imageUrl: item.imageUrl,
-        kind: 'shop',
-        mainText: item.name,
-      }}
-    />
+    <>
+      <section>
+        <h1 className={styles.sectionTitle}>내 가게</h1>
+        <ShopNoticeInfoBox
+          data={{
+            address1: item.address1,
+            description: item.description,
+            imageUrl: item.imageUrl,
+            kind: 'shop',
+            mainText: item.name,
+          }}
+        >
+          <div className={styles.buttonContainer}>
+            <Button buttonType='button' text='편집하기' isWhite size='M' />
+            <Button buttonType='button' text='공고 등록하기' size='M' />
+          </div>
+        </ShopNoticeInfoBox>
+        <div style={{ marginBottom: '12rem' }} />
+      </section>
+      <h1 className={styles.sectionTitle}>등록한 공고</h1>
+    </>
   );
 }
