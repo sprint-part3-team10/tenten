9f8ff08bfe4b95cde682132c7fe9cf3faf8206cd
Hooni07 <gkxogns21@gmail.com>
2024-04-23 18:47:27 +0900

    Feat: 로그인 동작 API 구현 및 cookie 저장 세팅

--- src/api/postLogin.ts
+++ src/api/postLogin.ts
@@ -1,14 +1,25 @@
+'use server';
+
+import { cookies } from 'next/headers';
 import { BASE_URL } from './api';
 
-interface LoginProps {
+interface UserProps {
   email: string;
-  password: string;
+  id: string;
+  type: 'employee' | 'employer';
+}
+
+interface LoginProps {
+  token: string;
+  user: UserProps;
 }
 
 const postLogin = async (
   email: string,
   password: string,
 ): Promise<LoginProps> => {
+  const cookieStore = cookies();
+
   const res = await fetch(`${BASE_URL}/token`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -17,7 +28,44 @@ const postLogin = async (
       password,
     }),
   });
-  return res.json();
+  const result = await res.json();
+
+  if (!res.ok) {
+    let errorMessage = '';
+    switch (res.status) {
+      case 404:
+        errorMessage = '존재하지 않거나 비밀번호가 일치하지 않습니다.';
+        break;
+      default:
+        errorMessage = '500이네요... 404로 되도록 해봐요';
+        break;
+    }
+    throw new Error(errorMessage);
+  }
+
+  const { item } = result;
+  const { token, user } = item;
+
+  cookieStore.set({
+    name: 'token',
+    value: token,
+    httpOnly: true,
+    path: '/',
+  });
+  cookieStore.set({
+    name: 'u_id',
+    value: user.item.id,
+    httpOnly: true,
+    path: '/',
+  });
+  cookieStore.set({
+    name: 'type',
+    value: user.item.type,
+    httpOnly: true,
+    path: '/',
+  });
+
+  return { token, user };
 };
 
 export default postLogin;
