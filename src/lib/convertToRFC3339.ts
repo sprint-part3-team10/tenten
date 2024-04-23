2d520041a4079c080e5d4a0e6981e4731512d63d
JA_X <envijs93@gmail.com>
2024-04-24 00:29:50 +0900

    Fix: RFC3339 변환 유틸함수 로직 수정

--- src/lib/convertToRFC3339.ts
+++ src/lib/convertToRFC3339.ts
@@ -1,10 +1,10 @@
 export default function convertToRFC3339(dateObject: Date) {
-  const year = dateObject.getUTCFullYear();
-  const month = String(dateObject.getUTCMonth() + 1).padStart(2, '0');
-  const day = String(dateObject.getUTCDate()).padStart(2, '0');
-  const hours = String(dateObject.getUTCHours()).padStart(2, '0');
-  const minutes = String(dateObject.getUTCMinutes()).padStart(2, '0');
-  const seconds = String(dateObject.getUTCSeconds()).padStart(2, '0');
+  const year = dateObject.getFullYear();
+  const month = String(dateObject.getMonth() + 1).padStart(2, '0');
+  const day = String(dateObject.getDate()).padStart(2, '0');
+  const hours = String(dateObject.getHours()).padStart(2, '0');
+  const minutes = String(dateObject.getMinutes()).padStart(2, '0');
+  const seconds = String(dateObject.getSeconds()).padStart(2, '0');
 
   return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
 }
