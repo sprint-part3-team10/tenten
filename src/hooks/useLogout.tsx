3a0e5fdd15ebd0b8a1835486653fee39ecea49fa
Hooni07 <gkxogns21@gmail.com>
2024-04-24 23:13:50 +0900

    Style: 주석 삭제

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -6,6 +6,4 @@ export default async function useLogout() {
   cookies().delete('u_id');
   cookies().delete('token');
   cookies().delete('userType');
-  // 여기서는 예시로 'authToken'라는 이름의 쿠키를 삭제합니다.
-  // 실제 쿠키 이름에 맞게 조정해주세요.
 }
