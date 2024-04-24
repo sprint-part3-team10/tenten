178f8153a29e9a10bff8df7276369648449e6952
eugene9851 <eugenelee980713@gmail.com>
2024-04-25 00:09:45 +0900

    Feat: 사장의 applyTable에서 승인/거절 버튼 클릭 시 모달&api연동 구현

--- src/components/applylist/ApplyTable.tsx
+++ src/components/applylist/ApplyTable.tsx
@@ -10,9 +10,13 @@ import { useEffect, useState } from 'react';
 import usePagination from '@/src/hooks/usePagination';
 import getUserApply from '@/src/api/getUserApply';
 import getShopApply from '@/src/api/getShopApply';
+import putAlarmStatus from '@/src/api/putAlarmStatus';
+import { useRouter } from 'next/navigation';
 import Label from './Label';
 import styles from './ApplyTable.module.scss';
 import Pagination from '../pagination/Pagination';
+import ModalPortal from '../common/modal/ModalPortal';
+import Modal from '../common/modal/Modal';
 
 interface ApplyTableProps {
   noticeId?: string;
@@ -27,10 +31,30 @@ const titleCol = {
 };
 
 function ApplyTable(props: ApplyTableProps) {
+  const router = useRouter();
   const { userType } = props;
 
   const [applies, setApplies] = useState([] as unknown);
   const [total, setTotal] = useState<number>(0);
+
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState('');
+  const [status, setStatus] = useState('');
+  const [applyId, setApplyId] = useState('');
+
+  const handleModalClick = (value: string, id: string) => {
+    setOpen(true);
+    const text =
+      value === 'reject' ? '신청을 거절하시겠어요?' : '신청을 승인하시겠어요?';
+    const newState = value === 'reject' ? 'rejected' : 'accepted';
+    setMessage(text);
+    setStatus(newState);
+    setApplyId(id);
+  };
+
+  const handleModal = (value: boolean) => {
+    setOpen(value);
+  };
 
   const LIMIT = 5;
   const { offset, selectedPage, handlePageChange } = usePagination(LIMIT);
@@ -46,83 +70,113 @@ function ApplyTable(props: ApplyTableProps) {
     };
     fetchData();
   }, [offset]);
+  console.log('applies', applies);
 
   return (
-    <div className={styles.tableContainer}>
-      <table className={styles.table}>
-        <thead>
-          <tr className={styles.titleRow}>
-            <th className={classNames(styles.title, styles.nameCol)}>
-              {titleCol[userType][0]}
-            </th>
-            <th className={classNames(styles.title, styles.timeCol)}>
-              {titleCol[userType][1]}
-            </th>
-            <th className={classNames(styles.title, styles.payCol)}>
-              {titleCol[userType][2]}
-            </th>
-            <th className={classNames(styles.title, styles.statusCol)}>
-              {titleCol[userType][3]}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {applies &&
-            applies.map(apply => (
-              <tr key={apply.item.id}>
-                <td className={classNames(styles.listRow, styles.nameCol)}>
-                  {userType === 'employee'
-                    ? apply.item.shop.item.name
-                    : apply.item.user.item.name}
-                </td>
-                <td className={classNames(styles.listRow, styles.timeCol)}>
-                  {userType === 'employee'
-                    ? formatDateAndTime(
-                        apply.item.notice.item.startsAt,
-                        apply.item.notice.item.workhour,
-                      )
-                    : apply.item.user.item.bio}
-                </td>
-                <td className={classNames(styles.listRow, styles.payCol)}>
-                  {userType === 'employee'
-                    ? formatWage(apply.item.notice.item.hourlyPay)
-                    : apply.item.user.item.phone}
-                </td>
-                <td className={classNames(styles.listRow, styles.statusCol)}>
-                  {userType === 'employee' ? (
-                    <Label labelType='status' content={apply.item.status} />
-                  ) : apply.item.status !== 'pending' ? (
-                    <Label labelType='status' content={apply.item.status} />
-                  ) : (
-                    <>
-                      <button className={classNames(styles.btn, styles.reject)}>
-                        거절하기
-                      </button>
-                      <button
-                        className={classNames(styles.btn, styles.approve)}
-                      >
-                        승인하기
-                      </button>
-                    </>
-                  )}
-                </td>
-              </tr>
-            ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan={4} className={styles.pagination}>
-              <Pagination
-                totalCount={total}
-                limit={LIMIT}
-                selectedPage={selectedPage}
-                handlePageChange={handlePageChange}
-              />
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
+    <>
+      <div className={styles.tableContainer}>
+        <table className={styles.table}>
+          <thead>
+            <tr className={styles.titleRow}>
+              <th className={classNames(styles.title, styles.nameCol)}>
+                {titleCol[userType][0]}
+              </th>
+              <th className={classNames(styles.title, styles.timeCol)}>
+                {titleCol[userType][1]}
+              </th>
+              <th className={classNames(styles.title, styles.payCol)}>
+                {titleCol[userType][2]}
+              </th>
+              <th className={classNames(styles.title, styles.statusCol)}>
+                {titleCol[userType][3]}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {applies &&
+              applies.map(apply => (
+                <tr key={apply.item.id}>
+                  <td className={classNames(styles.listRow, styles.nameCol)}>
+                    {userType === 'employee'
+                      ? apply.item.shop.item.name
+                      : apply.item.user.item.name}
+                  </td>
+                  <td className={classNames(styles.listRow, styles.timeCol)}>
+                    {userType === 'employee'
+                      ? formatDateAndTime(
+                          apply.item.notice.item.startsAt,
+                          apply.item.notice.item.workhour,
+                        )
+                      : apply.item.user.item.bio}
+                  </td>
+                  <td className={classNames(styles.listRow, styles.payCol)}>
+                    {userType === 'employee'
+                      ? formatWage(apply.item.notice.item.hourlyPay)
+                      : apply.item.user.item.phone}
+                  </td>
+                  <td className={classNames(styles.listRow, styles.statusCol)}>
+                    {userType === 'employee' ? (
+                      <Label labelType='status' content={apply.item.status} />
+                    ) : apply.item.status !== 'pending' ? (
+                      <Label labelType='status' content={apply.item.status} />
+                    ) : (
+                      <>
+                        <button
+                          className={classNames(styles.btn, styles.reject)}
+                          onClick={() =>
+                            handleModalClick('reject', apply.item.id)
+                          }
+                        >
+                          거절하기
+                        </button>
+                        <button
+                          className={classNames(styles.btn, styles.approve)}
+                          onClick={() =>
+                            handleModalClick('approve', apply.item.id)
+                          }
+                        >
+                          승인하기
+                        </button>
+                      </>
+                    )}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={4} className={styles.pagination}>
+                <Pagination
+                  totalCount={total}
+                  limit={LIMIT}
+                  selectedPage={selectedPage}
+                  handlePageChange={handlePageChange}
+                />
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      {open && (
+        <ModalPortal>
+          <Modal
+            icon='check'
+            handleButton={[
+              () => {},
+              () => {
+                putAlarmStatus(props.shopId, props.noticeId, applyId, status);
+                router.refresh();
+              },
+            ]}
+            message={message}
+            minWidth='29.8rem'
+            maxWidth='29.8rem'
+            buttonText={['아니오', '예']}
+            handleModal={handleModal}
+          />
+        </ModalPortal>
+      )}
+    </>
   );
 }
 
