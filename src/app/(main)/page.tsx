2df98089cee802f63f2fc32ccc137a308b3d3c07
ggjiny <jnss599@gmail.com>
2024-04-25 23:24:20 +0900

    Feat: 메인페이지에 CustomCardList 연결

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,19 @@
 import CardList from '@/src/components/card/CardList';
+import CustomCardList from '@/src/components/card/CustomCardList';
+import classNames from 'classnames';
+import { cookies } from 'next/headers';
 import styles from './page.module.scss';
 
 export default function Home() {
+  const userType = cookies().get('userType')?.value;
   return (
-    <div className={styles.mainLayout}>
-      <CardList />
+    <div>
+      {userType === 'employee' && <CustomCardList />}
+      <div
+        className={classNames({ [styles.withCustom]: userType === 'employer' })}
+      >
+        <CardList />
+      </div>
     </div>
   );
 }
