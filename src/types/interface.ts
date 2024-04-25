312dbec39145222eb8d588417fb93971d20be551
JA_X <envijs93@gmail.com>
2024-04-25 21:19:15 +0900

    Feat: MyShopFormData 공용interface 적용

---
+++ src/types/interface.ts
@@ -0,0 +1,9 @@
+export interface MyShopFormData {
+  name: string;
+  category: string;
+  address1: string;
+  address2: string;
+  description: string;
+  imageUrl: string;
+  originalHourlyPay: number | string;
+}
