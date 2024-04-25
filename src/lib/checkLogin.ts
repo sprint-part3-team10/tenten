052b83d2b55e91c90f9623576da83447f535ce11
Hooni07 <gkxogns21@gmail.com>
2024-04-25 18:52:08 +0900

    Style: 불필요한 조건문 제거

--- src/lib/checkLogin.ts
+++ src/lib/checkLogin.ts
@@ -2,18 +2,15 @@ import { cookies } from 'next/headers';
 
 interface CheckCookieData {
   hasToken: boolean;
-  userType?: 'employee' | 'employer';
+  userType: 'employee' | 'employer';
 }
 
 function checkLogin(): CheckCookieData {
   const cookiesStore = cookies();
   const hasToken = cookiesStore.has('token');
-  if (cookiesStore.get('userType')) {
-    const userType = cookiesStore.get('userType').value;
-    return { hasToken, userType };
-  }
+  const userType = cookiesStore.get('userType').value;
 
-  return { hasToken };
+  return { hasToken, userType };
 }
 
 export default checkLogin;
