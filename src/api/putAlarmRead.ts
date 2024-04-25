d87e4e426c56d35a6338c96a07ac12bb4fd0fa40
eugene9851 <eugenelee980713@gmail.com>
2024-04-25 22:41:44 +0900

    Feat: putAlarmRead api 구현

---
+++ src/api/putAlarmRead.ts
@@ -0,0 +1,18 @@
+import { BASE_URL } from './api';
+
+const putAlarmRead = async (userId: string, alertId: string) => {
+  const res = await fetch(`${BASE_URL}/users/${userId}/alerts/${alertId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization:
+        'Bearer ' +
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIwNjZmMDgwYy01MjY1LTRiNzAtODM2ZS0wZjEzNjBiNTcwMTAiLCJpYXQiOjE3MTQwMjMxMjZ9.tAnHykJc6znAkv26ntNx6-GTHuHXLd5DOz9VecWyEGA',
+    },
+    cache: 'no-store',
+  });
+  const result = await res.json();
+  return result;
+};
+
+export default putAlarmRead;
