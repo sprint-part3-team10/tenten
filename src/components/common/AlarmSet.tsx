cdac3a1534cfc0ff677b240f2c2b3b2443e07274
eugene9851 <eugenelee980713@gmail.com>
2024-04-25 18:19:58 +0900

    Feat: 알람 배열 map으로 구현

--- src/components/common/AlarmSet.tsx
+++ src/components/common/AlarmSet.tsx
@@ -2,18 +2,23 @@
 
 import alarmsRead from '@/public/icons/alarmsRead.svg';
 import alarmsNotRead from '@/public/icons/alarmsNotRead.svg';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import useOutsideClick from '@/src/hooks/useOutsideClick';
 import Alarms from '@/src/components/common/Alarms';
-import styles from './AlarmSet.module.scss';
 import classNames from 'classnames';
+import getAlarms from '@/src/api/getAlarms';
+import styles from './AlarmSet.module.scss';
 
 export default function AlarmSet() {
-  const notRead = 1;
-  const alarmIcon = notRead ? alarmsNotRead : alarmsRead;
-  const ref = useRef<HTMLDivElement>(null);
+  const userId = '066f080c-5265-4b70-836e-0f1360b57010';
+
   const [isOpen, setIsOpen] = useState(false);
+  const [unRead, setUnRead] = useState(0);
+  const [alarms, setAlarms] = useState([]);
+
+  const alarmIcon = unRead ? alarmsNotRead : alarmsRead;
+  const ref = useRef<HTMLDivElement>(null);
 
   const handleCloseClick = () => {
     setIsOpen(false);
@@ -24,17 +29,32 @@ export default function AlarmSet() {
 
   useOutsideClick(ref, handleCloseClick);
 
+  useEffect(() => {
+    const getUnread = async () => {
+      const { items } = await getAlarms(userId);
+      const count = items.map(item => item.read === false).length;
+      setUnRead(count);
+      setAlarms([...items]);
+    };
+    getUnread();
+  }, [unRead]);
+
   return (
     <div className={styles.button}>
-      <div onClick={handleOpenAlarms} ref={ref}>
+      <div onClick={handleOpenAlarms}>
         <Image src={alarmIcon} width={24} height={24} alt='알람확인아이콘' />
       </div>
       <div
         className={classNames(styles.alarms, {
           [styles.closeAlarm]: !isOpen,
         })}
+        ref={ref}
       >
-        <Alarms handleCloseClick={handleCloseClick} />
+        <Alarms
+          handleCloseClick={handleCloseClick}
+          unRead={unRead}
+          alarms={alarms}
+        />
       </div>
     </div>
   );
