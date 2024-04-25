dc11eceb71633d0ebc5ee86dcde5d26363d330fb
Hooni07 <gkxogns21@gmail.com>
2024-04-25 19:06:59 +0900

    Style: shop_id -> s_id 변경

--- src/lib/deleteCookie.ts
+++ src/lib/deleteCookie.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers';
 
 async function deleteCookie() {
   cookies().delete('u_id');
-  cookies().delete('shop_id');
+  cookies().delete('s_id');
   cookies().delete('token');
   cookies().delete('userType');
 }
