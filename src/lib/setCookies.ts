4ec85b5369c07bbe52a8805274073b614d89ceb8
Hooni07 <gkxogns21@gmail.com>
2024-04-26 14:08:21 +0900

    Feat: token, shopId, userId, userType을 한번에 관리하는 파일 생성

---
+++ src/lib/setCookies.ts
@@ -0,0 +1,59 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+interface UserData {
+  email: string;
+  id: string;
+  type: 'employee' | 'employer';
+}
+
+interface CookieData {
+  token?: string;
+  user?: UserData;
+  shopId?: string;
+}
+
+function setCookies({ token, user, shopId }: CookieData) {
+  const cookieStore = cookies();
+  const expireTime = 12 * 60 * 60 * 1000;
+
+  if (token) {
+    cookieStore.set({
+      name: 'token',
+      value: token,
+      httpOnly: true,
+      path: '/',
+      expires: Date.now() + expireTime,
+    });
+  }
+
+  if (user) {
+    cookieStore.set({
+      name: 'u_id',
+      value: user.id,
+      httpOnly: true,
+      path: '/',
+      expires: Date.now() + expireTime,
+    });
+    cookieStore.set({
+      name: 'userType',
+      value: user.type,
+      httpOnly: true,
+      path: '/',
+      expires: Date.now() + expireTime,
+    });
+  }
+
+  if (shopId) {
+    cookieStore.set({
+      name: 's_id',
+      value: shopId,
+      httpOnly: true,
+      path: '/',
+      expires: Date.now() + expireTime,
+    });
+  }
+}
+
+export default setCookies;
