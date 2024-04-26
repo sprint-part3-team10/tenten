c04a6f7468f64830e5d01e3863925997c804f4ce
Hooni07 <gkxogns21@gmail.com>
2024-04-26 16:54:02 +0900

    Feat: 로그아웃 시, modal을 통한 확인 기능 추가

--- src/components/common/LogoutButton.tsx
+++ src/components/common/LogoutButton.tsx
@@ -2,14 +2,42 @@
 
 import deleteCookie from '@/src/lib/deleteCookie';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import ModalPortal from './modal/ModalPortal';
+import Modal from './modal/Modal';
 
 export default function LogoutButton() {
   const router = useRouter();
+  const [isShow, setIsShow] = useState(false);
+  const [modalMessage, setModalMessage] = useState('로그아웃 하시겠습니까?');
+
+  const handleShowModal = (modalState: boolean) => {
+    setIsShow(modalState);
+  };
+
   const handleLogout = async () => {
     await deleteCookie();
     router.push('/');
     router.refresh();
   };
 
-  return <button onClick={handleLogout}>로그아웃</button>;
+  return (
+    <>
+      <button onClick={handleShowModal}>로그아웃</button>
+      {isShow && (
+        <ModalPortal>
+          <Modal
+            icon='warning'
+            message={modalMessage}
+            minWidth='20rem'
+            maxWidth='45rem'
+            buttonText={['로그아웃']}
+            buttonWidthPercent='25%'
+            handleModal={handleShowModal}
+            handleButton={[handleLogout]}
+          />
+        </ModalPortal>
+      )}
+    </>
+  );
 }
