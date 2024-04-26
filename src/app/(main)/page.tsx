07331d75e0ab020fe88b1f4905ea04709561b619
ggjiny <jnss599@gmail.com>
2024-04-26 16:00:53 +0900

    Style: getCookie 사용

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,18 @@
 import CardList from '@/src/components/card/CardList';
 import CustomCardList from '@/src/components/card/CustomCardList';
 import classNames from 'classnames';
-import { cookies } from 'next/headers';
+import getCookie from '@/src/lib/getCookie';
 import styles from './page.module.scss';
 
 export default function Home() {
-  const userType = cookies().get('userType')?.value;
+  const userType = getCookie('userType');
+
   return (
     <div>
       {userType === 'employee' && <CustomCardList />}
       <div
         className={classNames({
-          [styles.noCustom]: userType === 'employer' || userType === undefined,
+          [styles.noCustom]: userType !== 'employee',
         })}
       >
         <CardList />
