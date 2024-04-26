54449e78815d07b9683d3b7170116a7ea836f959
hanbin <rj2303@naver.com>
2024-04-26 20:34:48 +0900

    Style: 주석 제거

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,13 +11,7 @@ export default function NotFound() {
         <Image src={notfound} width={500} height={300} alt='404페이지' />
       </div>
       <h1 className='errorTitle'>404 Not Found</h1>
-      <Image
-        src={warning}
-        alt='느낌표 아이콘'
-        width={60}
-        height={80}
-        // style={{ marginTop: '8rem' }}
-      />
+      <Image src={warning} alt='느낌표 아이콘' width={60} height={80} />
       <p className='errorMessage'>해당 페이지를 찾을 수 없어요.</p>
       <Link href='/' className='returnHome'>
         홈으로 돌아가기
