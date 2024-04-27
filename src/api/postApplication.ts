968a74b660bfc9a85accecae8ef044896e3a354d
hanbin <rj2303@naver.com>
2024-04-27 11:56:20 +0900

    Feat: 공고 지원 api 에러 분류

--- src/api/postApplication.ts
+++ src/api/postApplication.ts
@@ -16,6 +16,25 @@ const postApplication = async (
     },
   );
 
+  if (!res.ok) {
+    let errorMessage = '';
+    switch (res.status) {
+      case 400:
+        errorMessage = '공고가 마감되었습니다';
+        break;
+      case 401:
+        errorMessage = '로그인이 필요합니다';
+        break;
+      case 404:
+        errorMessage = '가게나 공고, 사용자가 존재하지 않습니다';
+        break;
+      default:
+        errorMessage = `${res.status} ${res.statusText}`;
+        break;
+    }
+    throw new Error(errorMessage);
+  }
+
   return res.json();
 };
 
