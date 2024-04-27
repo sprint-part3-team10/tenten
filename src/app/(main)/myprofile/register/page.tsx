84405cfc0b930215882728b8b9cbe8cb2160801e
JA_X <envijs93@gmail.com>
2024-04-28 04:30:42 +0900

    Fix: 내 프로필 등록되어 있을 시 내 프로필 등록 페이지 접근 불가

--- src/app/(main)/myprofile/register/page.tsx
+++ src/app/(main)/myprofile/register/page.tsx
@@ -1,10 +1,16 @@
+import { redirect } from 'next/navigation';
+import getProfileData from '@/src/api/getProfileData';
 import MyProfileRegister from '@/src/components/myProfile/MyProfileRegister';
 import getCookie from '@/src/lib/getCookie';
 import styles from './page.module.scss';
 
-export default function Home() {
+export default async function Home() {
   const accessToken = getCookie('token');
   const userId = getCookie('u_id');
+  const { name } = await getProfileData(userId);
+
+  if (name) redirect('/');
+
   return (
     <div className={styles.layout}>
       <MyProfileRegister token={accessToken} userId={userId} />
