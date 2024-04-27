7f422bb9f4fd20f771e685127e40ba29f4e8834a
JA_X <envijs93@gmail.com>
2024-04-28 05:36:03 +0900

    Fix: redirect 설정 시 편집페이지 진입 시에도 루트 페이지로 이동하는 부수효과 방지를 위해 csr 페이지 안에서 router 되게 수정

--- src/app/(main)/myprofile/register/page.tsx
+++ src/app/(main)/myprofile/register/page.tsx
@@ -1,5 +1,3 @@
-import { redirect } from 'next/navigation';
-import getProfileData from '@/src/api/getProfileData';
 import MyProfileRegister from '@/src/components/myProfile/MyProfileRegister';
 import getCookie from '@/src/lib/getCookie';
 import styles from './page.module.scss';
@@ -7,9 +5,6 @@ import styles from './page.module.scss';
 export default async function Home() {
   const accessToken = getCookie('token');
   const userId = getCookie('u_id');
-  const { name } = await getProfileData(userId);
-
-  if (name) redirect('/');
 
   return (
     <div className={styles.layout}>
