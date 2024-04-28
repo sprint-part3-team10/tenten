03819a7cccf7eaeac316f31c85c8360cb153deb7
Hooni07 <gkxogns21@gmail.com>
2024-04-28 18:47:47 +0900

    Refactor: userType에 대한 undefined 케이스 추가 및 type 수정

--- src/lib/checkLogin.ts
+++ src/lib/checkLogin.ts
@@ -3,13 +3,17 @@ import getCookie from './getCookie';
 
 interface CheckCookieData {
   hasToken: boolean;
-  userType?: 'employee' | 'employer';
+  userType?: 'employee' | 'employer' | undefined;
 }
 
 function checkLogin(): CheckCookieData {
   const cookiesStore = cookies();
   const hasToken = cookiesStore.has('token');
-  const userType = getCookie('userType');
+  let userType: string | undefined = getCookie('userType');
+
+  if (userType !== 'employee' && userType !== 'employer') {
+    userType = undefined;
+  }
 
   return { hasToken, userType };
 }
