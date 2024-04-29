8b21412f610184fa1c59351e2662dfa411f0f00f
hanbin <rj2303@naver.com>
2024-04-29 18:11:18 +0900

    Fix: 쿼리 시간값 이전 상태로 복구

--- src/lib/convertToRFC3339.ts
+++ src/lib/convertToRFC3339.ts
@@ -14,7 +14,7 @@ export default function convertToRFC3339(
     return `${year}-${month}-${day}T${initTime}`;
 
   if (init) {
-    const hours = String(now.getHours() + 10).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
 
