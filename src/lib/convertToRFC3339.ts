d4d335afe8eff4fca0f6ab37e9e9966d46ebb878
hanbin <rj2303@naver.com>
2024-04-29 18:23:37 +0900

    Fix: api 시간값 수정

--- src/lib/convertToRFC3339.ts
+++ src/lib/convertToRFC3339.ts
@@ -14,9 +14,9 @@ export default function convertToRFC3339(
     return `${year}-${month}-${day}T${initTime}`;
 
   if (init) {
-    const hours = String(now.getHours()).padStart(2, '0');
+    const hours = String(now.getHours() + 9).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds() + 5).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
 
     return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
   }
