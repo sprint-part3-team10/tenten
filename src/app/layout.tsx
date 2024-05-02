3499a4e66275451de15e0be09af558248a2eed6a
eugene9851 <eugenelee980713@gmail.com>
2024-05-03 00:02:06 +0900

    Fix: opengraph image 크기 조절

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,20 @@ import type { Metadata } from 'next';
 import '@/src/app/globals.scss';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://synergy10.vercel.app/'),
   title: 'Synergy',
   description: 'codeit sprint part3 tenten',
   icons: {
     icon: '/icons/favicon.ico',
+  },
+  openGraph: {
+    images: [
+      {
+        url: '/icons/logo.png',
+        width: 800,
+        height: 400,
+      },
+    ],
   },
 };
 
