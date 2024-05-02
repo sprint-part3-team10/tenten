e8396ce812c1b4dfa527ef5782086de5b315942f
hanbin <rj2303@naver.com>
2024-05-02 18:36:26 +0900

    Feat: URL 복사 기능 구현

--- src/components/share/Share.tsx
+++ src/components/share/Share.tsx
@@ -3,26 +3,42 @@
 import Image from 'next/image';
 import shareIcon from '@/public/icons/share.svg';
 import { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import styles from './Share.module.scss';
+import Toast from '../common/toast/Toast';
 
 function Share() {
   const [isOpen, setIsOpen] = useState(false);
+  const [toast, setToast] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <div className={styles.button} onClick={handleClick}>
-      <Image src={shareIcon} alt='공유하기' width={20} height={20} />
-      {isOpen && (
-        <ul className={styles.list}>
-          <li>URL 복사</li>
-          <li>카카오톡</li>
-          <li>페이스북</li>
-        </ul>
-      )}
-    </div>
+    <>
+      <div className={styles.button} onClick={handleClick}>
+        <Image src={shareIcon} alt='공유하기' width={20} height={20} />
+        {isOpen && (
+          <ul className={styles.list}>
+            <CopyToClipboard
+              text={window.location.href}
+              onCopy={() => {
+                setToast(true);
+                setTimeout(() => {
+                  setToast(false);
+                }, 1700);
+              }}
+            >
+              <li>URL 복사</li>
+            </CopyToClipboard>
+            <li>카카오톡</li>
+            <li>페이스북</li>
+          </ul>
+        )}
+      </div>
+      {toast && <Toast message='URL이 복사되었습니다.' />}
+    </>
   );
 }
 
